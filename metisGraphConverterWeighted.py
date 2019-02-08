42f2d2640c159e0f514f6e0dbf32fdefce5c6ce2
Demian Hespe <hespe@kit.edu>
2019-02-08 16:07:38 +0100

    Add Converter to weighted metis graph format

---
+++ metisGraphConverterWeighted.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+graphFile = ""
+
+# print(sys.argv)
+
+for arg in sys.argv:
+    # print(arg)
+    if arg.endswith(".hgr"):
+        graphFile = arg
+        break
+
+
+f = open(graphFile, 'r')
+
+
+n = -1
+
+for line in f:
+    if line.startswith("p td"):
+        n = int(line.split()[2])
+        break
+
+edges = [[] for i in range(n)]
+
+m = 0
+
+for line in f:
+    if not (line.startswith("p") or line.startswith("c")):
+        u = int(line.split()[0]) - 1
+        v = int(line.split()[1]) - 1
+        if u == v:
+            continue
+        if not v in edges[u]:
+            edges[u].append(v)
+            m += 1
+        if not u in edges[v]:
+            edges[v].append(u)
+            m += 1
+
+m /= 2
+
+for line in edges:
+    line.sort()
+
+f.close()
+
+f = open(os.path.join("metisGraphsWeighted", os.path.basename(graphFile) + ".graph"), 'w')
+
+f.write("%s %s 10\n" % (n, m))
+
+for u in range(n):
+    line = "1"
+    for v in edges[u]:
+        line += " " + str(v + 1)
+    line = line.strip(' ')
+    line += "\n"
+    f.write(line)
+
+f.close()
