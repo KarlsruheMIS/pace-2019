b724dacbc00a04c400057da934f5ce0c503dda93
Demian Hespe <hespe@kit.edu>
2019-04-26 16:51:12 +0200

    Call executable from parallel script

--- run_parallel_clique.sh
+++ run_parallel_clique.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-parallel -j32 --timeout 7200 --joblog parallel_log ./scripts/runClique.sh {} '>' output/{.}.output ::: graphs/*
+parallel -j32 --timeout 7200 --joblog parallel_log ./optimized/kernelizeAndMakeCliqueInstance {} '>' output/{.}.output ::: graphs/*
--- scripts/runClique.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-app="_kernel"
-
-./optimized/kernelizeAndMakeCliqueInstance $1
-./extern/cliqueSolver/a.out $1$app
-rm $1$app
