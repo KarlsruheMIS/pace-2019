d34be682afd9940dd0034903f2bd4c17e6a0d87e
Demian Hespe <hespe@kit.edu>
2019-04-29 13:40:07 +0200

    Modify run script to do (almost) what optil would do

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-parallel -j32 --timeout 7200 --joblog parallel_log ./optimized/vc_solver {} --time_limit=7200 --console_log '>' output/{.}.output ::: graphs/*
+parallel -j32 --timeout 1800 --joblog parallel_log ./optimized/vc_solver {} '>' output/{.}.output ::: graphs/*
